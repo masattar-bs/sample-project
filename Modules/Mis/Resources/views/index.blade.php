21b5710415d9574c08869d98fe155b051d5633f3
masattar-bs <abdus.sattar@bs-23.net>
2018-12-18 18:03:28 +0600

    commiting after changing the views in mis

--- Modules/Mis/Resources/views/index.blade.php
+++ Modules/Mis/Resources/views/index.blade.php
@@ -1,6 +1,12 @@
 @extends('mis::layouts.master')
 
 @section('content')
+    <div class="pull-right">
+        <ul class="nav navbar">
+            <li><a href="../mis">MIS Home</a></li>
+            <li><a href="/mis/check">MIS Check</a></li>
+        </ul>
+    </div>
     <center>
         <h1>Hello World :)</h1>
 
