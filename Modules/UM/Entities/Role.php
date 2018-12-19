b78ebf3d29eb9e320414a9dda5567d5bb717a595
masattar-bs <abdus.sattar@bs-23.net>
2018-12-19 13:52:07 +0600

    commiting after UM module integration

---
+++ Modules/UM/Entities/Role.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\UM\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    protected $fillable = [];
+}
---
+++ Modules/UM/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('um::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('um.name') !!}
+    </p>
+@stop
